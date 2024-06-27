b2a2e38b13deb0f158af88da890c1d0947a2fd2a
ju1icyju <76118538+ju1icyju@users.noreply.github.com>
2024-06-27 18:03:48 +0300

    Update settings.py

--- video/settings.py
+++ video/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c(2(d7h9&+apl(k!thgmrkwo(x#7jgj_jycd(xyf5(+b0tbhu5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
