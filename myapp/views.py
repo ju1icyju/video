3417d703906e8a641d04f6ed6c58e5c463ecbc0b
ju1icyju <76118538+ju1icyju@users.noreply.github.com>
2024-06-28 01:37:52 +0300

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -11,15 +11,15 @@ def create_video(request):
         video_path = os.path.join(settings.STATIC_ROOT, 'video/video_test.mp4')
         video = VideoFileClip(video_path)                                                                               #  изначальное видео на которое ссылаемся
 
-        txt_clip = ( TextClip(text,fontsize=30,color='white')
-                   .set_position(lambda t: (max(0, video.w - 50*t), 'center'))
-                   .set_duration(video.duration)
-                   .crossfadein(0.5)
-                   .crossfadeout(0.5)
-                   .set_start(0) )
-                                                                                                                        # настройка текста
+        txt_clip = (TextClip(text,fontsize=30,color='white')
+                    .set_position(lambda t: (video.w - 50 * t, 'center'))
+                    .set_duration(video.duration)
+                    .crossfadein(0.5)                                                                                   # настройка текста
+                    .crossfadeout(0.5)
+                    .set_start(0))
+
         video = CompositeVideoClip([video, txt_clip], size=(100, 100))
         output_path = os.path.join(settings.STATIC_ROOT, 'video/video_output.mp4')
-        video.write_videofile(output_path, codec="mpeg4")
+        video.write_videofile(output_path, codec="libx264")
         return render(request, 'video_created.html', {'text': text})
                                                                                                                         # склеиваем и сохраняем
